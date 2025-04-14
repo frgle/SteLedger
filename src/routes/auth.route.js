ea2125788b50b322bedfb0a3f29e78c8d7e893a3
frgle <frglemelito@gmail.com>
2025-04-14 17:33:42 -0400

    solving issues

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -9,6 +9,6 @@ router.post('/register', (req, res) => authController.register(req, res));
 router.post('/login', (req, res) => authController.login(req, res));
 router.post('/logout', (req, res) => authController.logout(req, res));
 router.get('/token-info', (req, res) => authController.tokenInfo(req, res));
-router.get('/me', (req, res) => res.json(req.user.profile));
+router.get('/profile', (req, res) => res.json(req.user.profile));
 
 export default router;
